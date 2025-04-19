2a7a80d42f7580e4975195461f035da2369d2d93
Manikanta-Naraharasetti <naraharasettishivamanikanta@gmail.com>
2025-04-19 15:17:49 +0530

    Resolved an issue by making flask_app to app

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import request,render_template,jsonify,Flask
 import pickle
 
 
-flask_app=Flask(__name__)
+app=Flask(__name__)
 model=pickle.load(open("model.pkl","rb"))
 
-@flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict",methods=["post"])
+@app.route("/predict",methods=["post"])
 def predict():
     float_feature=[float(x) for x in request.form.values()]
     features=[np.array(float_feature)]
@@ -18,4 +18,4 @@ def predict():
     return render_template("index.html", Predicted_text=f"The Predicted Crop is {prediction[0]}")
 
 if __name__=="__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
